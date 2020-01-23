66618926ec2e50b01e216ee3a9165f7bb2c592f0
mangesh <mlatekar@gmail.com>
2020-01-23 11:14:07 +0530

    MODIFIED:use scale  to take decimal value

--- Arithmetic_computation.sh
+++ Arithmetic_computation.sh
@@ -2,8 +2,12 @@ echo "Welcome to Arithmetic operation"
 read -p "Enter three numbers : " a b c
 echo "a=$a b=$b c=$c"
 
-operation1=$(($a+$b*$c))
+operation1=`echo "scale=2;($a+($b*$c))" | bc`;
 echo "a+b*c : "$operation1
 
-operation2=$(($a*$b+$c))
+operation2=`echo "scale=2;($a*($b+$c))" | bc`;
 echo "a*b+c : "$operation2
+
+operation3=`echo "scale=2;($c+($a/$b))" | bc`;
+echo "c+a/b :" $operation3
+
