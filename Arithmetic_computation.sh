bd3e9d81de1be77aff44b4820f6b7adada46ffb8
mangesh <mlatekar@gmail.com>
2020-01-23 11:26:59 +0530

    ADDED:a%b+c

--- Arithmetic_computation.sh
+++ Arithmetic_computation.sh
@@ -11,3 +11,6 @@ echo "a*b+c : "$operation2
 operation3=`echo "scale=2;($c+($a/$b))" | bc`;
 echo "c+a/b :" $operation3
 
+operation4=`echo "scale=2;(($a%$b)+$c)" | bc`;
+echo "a%b+c :" $operation4
+
