2daf2fc8bc1b91919d5b4c532c636c61d1c14b99
mangesh <mlatekar@gmail.com>
2020-01-23 14:24:50 +0530

    MODIFIED:use function and array

--- Arithmetic_computation.sh
+++ Arithmetic_computation.sh
@@ -3,15 +3,24 @@ read -p "Enter three numbers : " a b c
 echo "a=$a b=$b c=$c"
 declare -A operation
 
-operation[operation1]=`echo "scale=2;($a+($b*$c))" | bc`;
-echo "a+b*c : "${operation[operation1]}
+function result() {
+for (( i=0; i<=4; i++))
+do
+array[$i]=${operation[operation$((i+1))]}
+done   
+}
 
-operation[operation2]=`echo "scale=2;($a*($b+$c))" | bc`;
-echo "a*b+c : "${operation[operation2]}
+operation[operation1]=`echo "scale=2;($a+($b*$c))" | bc`;
+#echo "a+b*c : "${operation[operation1]}
+
+operation[operation2]=`echo "scale=2;(($a*$b)+$c)" | bc`;
+#echo "a*b+c : "${operation[operation2]}
 
 operation[operation3]=`echo "scale=2;($c+($a/$b))" | bc`;
-echo "c+a/b :" ${operation[operation3]}
+#echo "c+a/b :" ${operation[operation3]}
 
 operation[operation4]=`echo "scale=2;(($a%$b)+$c)" | bc`;
-echo "a%b+c :" ${operation[operation4]}
+#echo "a%b+c :" ${operation[operation4]}
 
+result
+echo ${array[@]}
