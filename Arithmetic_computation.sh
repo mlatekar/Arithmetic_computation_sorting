c112a381a5f4c867d48dfccb0230ff81377f9ab3
mangesh <mlatekar@gmail.com>
2020-01-23 11:47:10 +0530

    ADDED:used dictionary

--- Arithmetic_computation.sh
+++ Arithmetic_computation.sh
@@ -1,16 +1,17 @@
 echo "Welcome to Arithmetic operation"
 read -p "Enter three numbers : " a b c
 echo "a=$a b=$b c=$c"
+declare -A operation
 
-operation1=`echo "scale=2;($a+($b*$c))" | bc`;
-echo "a+b*c : "$operation1
+operation[operation1]=`echo "scale=2;($a+($b*$c))" | bc`;
+echo "a+b*c : "${operation[operation1]}
 
-operation2=`echo "scale=2;($a*($b+$c))" | bc`;
-echo "a*b+c : "$operation2
+operation[operation2]=`echo "scale=2;($a*($b+$c))" | bc`;
+echo "a*b+c : "${operation[operation2]}
 
-operation3=`echo "scale=2;($c+($a/$b))" | bc`;
-echo "c+a/b :" $operation3
+operation[operation3]=`echo "scale=2;($c+($a/$b))" | bc`;
+echo "c+a/b :" ${operation[operation3]}
 
-operation4=`echo "scale=2;(($a%$b)+$c)" | bc`;
-echo "a%b+c :" $operation4
+operation[operation4]=`echo "scale=2;(($a%$b)+$c)" | bc`;
+echo "a%b+c :" ${operation[operation4]}
 
