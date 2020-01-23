6113db5f85bdd38ffde752b3d342408a6c8d61ac
mangesh <mlatekar@gmail.com>
2020-01-23 16:08:56 +0530

    ADDED:sorting_algo_descending

--- Arithmetic_computation.sh
+++ Arithmetic_computation.sh
@@ -4,23 +4,41 @@ echo "a=$a b=$b c=$c"
 declare -A operation
 
 function result() {
-for (( i=0; i<=4; i++))
-do
-array[$i]=${operation[operation$((i+1))]}
-done   
+	for (( i=0; i<4; i++))
+	do
+		array[$i]=${operation[operation$((i+1))]}
+	done   
 }
-
+function descending(){
+	for (( i=0; i<$size; i++ ))
+	do
+		for (( j=i+1; j<=$size; j++ ))
+		do
+			if [[ ${array[i]%%.*} -lt ${array[j]%%.*} ]]
+			then
+				temp=${array[i]}	
+				array[i]=${array[j]}
+				array[j]=$temp
+			fi
+		done
+	done
+}
 operation[operation1]=`echo "scale=2;($a+($b*$c))" | bc`;
-#echo "a+b*c : "${operation[operation1]}
+echo "a+b*c : " ${operation[operation1]}
 
 operation[operation2]=`echo "scale=2;(($a*$b)+$c)" | bc`;
-#echo "a*b+c : "${operation[operation2]}
+echo "a*b+c : "${operation[operation2]}
 
 operation[operation3]=`echo "scale=2;($c+($a/$b))" | bc`;
-#echo "c+a/b :" ${operation[operation3]}
+echo "c+a/b :" ${operation[operation3]}
 
 operation[operation4]=`echo "scale=2;(($a%$b)+$c)" | bc`;
-#echo "a%b+c :" ${operation[operation4]}
+echo "a%b+c :" ${operation[operation4]}
 
-result
-echo ${array[@]}
+
+
+result   #call the function to store result in array
+echo  "using array print values "${array[@]} #print the result 
+size=${#array[@]}
+descending
+echo "result in desending order : "${array[@]}
