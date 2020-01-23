916a5f9ae8f2889f3c810cda18f448db15d8e8bd
mangesh <mlatekar@gmail.com>
2020-01-23 17:14:55 +0530

    ADDED:sort_ascending_order

--- Arithmetic_computation.sh
+++ Arithmetic_computation.sh
@@ -23,22 +23,40 @@ function descending(){
 		done
 	done
 }
+function Ascending(){
+   for (( i=0; i<$size; i++ ))
+   do
+      for (( j=i+1; j<=$size; j++ ))
+      do
+         if [[ ${array[i]%%.*} -gt ${array[j]%%.*} ]]
+         then
+            temp=${array[i]}  
+            array[i]=${array[j]}
+            array[j]=$temp
+         fi
+      done
+   done
+}
+
 operation[operation1]=`echo "scale=2;($a+($b*$c))" | bc`;
-echo "a+b*c : " ${operation[operation1]}
+echo "a+b*c : "${operation[operation1]}
 
 operation[operation2]=`echo "scale=2;(($a*$b)+$c)" | bc`;
 echo "a*b+c : "${operation[operation2]}
 
 operation[operation3]=`echo "scale=2;($c+($a/$b))" | bc`;
-echo "c+a/b :" ${operation[operation3]}
+echo "c+a/b : "${operation[operation3]}
 
 operation[operation4]=`echo "scale=2;(($a%$b)+$c)" | bc`;
-echo "a%b+c :" ${operation[operation4]}
+echo "a%b+c : "${operation[operation4]}
 
 
 
 result   #call the function to store result in array
-echo  "using array print values "${array[@]} #print the result 
+echo "using array print values  : "${array[@]} #print the result 
 size=${#array[@]}
 descending
 echo "result in desending order : "${array[@]}
+Ascending
+echo "result in Ascending order :"${array[@]}
+
